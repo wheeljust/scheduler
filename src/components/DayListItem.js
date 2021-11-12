70d7cc82bb30e0da03e221bb0ea6c5103124c3db
Justin Wheeler <justinwwheeler@gmail.com>
2021-11-12 14:59:46 -0700

    refactor to show spots remaining in correct format

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames"
 
-import "components/DayListItem.scss";
+import "./DayListItem.scss";
 
 export default function DayListItem(props) {
   let dayClass = classNames("day-list__item", {
@@ -9,13 +9,17 @@ export default function DayListItem(props) {
     "day-list__item--full": !props.spots
   });
 
+  const formatSpots = () => {
+    return (props.spots) ? `${props.spots} spot${(props.spots > 1) ? "s" : ""} remaining` : `no spots remaining`;
+  }
+
   return (
     <li
       onClick={() => props.setDay(props.name)}
       className={dayClass}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
