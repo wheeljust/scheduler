fba0c1986daec8a80929f9a16c3eeed22bca511c
Justin Wheeler <justinwwheeler@gmail.com>
2021-11-13 15:29:35 -0700

    updated comments

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,7 +7,7 @@ export default function InterviewerList(props) {
   //Extract the props to variables to keep code DRY
   const { interviewers, interviewer, setInterviewer } = props;
 
-  //.map over the days array to create a new array called dayList containing all of the DayListItem components
+  //.map over the interviewers array to create a new array called interviewerList containing all of the InterviewerListItem components
   const interviewerList = interviewers.map((currentInterviewer) => {
 
     const interviewerProps = {
@@ -22,7 +22,7 @@ export default function InterviewerList(props) {
 
   });
 
-  //return the array of the DayListItem components as a list
+  //return the array of the components as a list with a heading
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
