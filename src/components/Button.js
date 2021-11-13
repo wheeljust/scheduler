61f21de0408f0c9ab3c28ffc4af9e3566616174e
Justin Wheeler <justinwwheeler@gmail.com>
2021-11-13 14:03:51 -0700

    refactor props using obj destructuring

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,16 +5,18 @@ import "./Button.scss";
 
 export default function Button(props) {
 
+  const { confirm, danger, onClick, disabled } = props;
+
   let buttonClass = classNames("button", {
-    "button--confirm": props.confirm,
-    "button--danger": props.danger
+    "button--confirm": confirm,
+    "button--danger": danger
   });
 
   return (
     <button
       className={buttonClass}
-      onClick={props.onClick}
-      disabled={props.disabled}
+      onClick={onClick}
+      disabled={disabled}
     >
       {props.children}
     </button>
