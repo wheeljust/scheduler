3b60a08fc784ca9ee4383f617a7b3f6149f6ed4d
Justin Wheeler <justinwwheeler@gmail.com>
2021-11-15 20:23:25 -0700

    add appointment list to the home page, include a sample appointment data set that will be replaced by API in future implementation

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList"
+import Appointment from "components/Appointment"
 
 const days = [
   {
@@ -21,8 +22,60 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const showAppointments = () => {
+    const appointmentList = Object.values(appointments).map((appointment) => {
+      return <Appointment
+        key={appointment.id}
+        {...appointment}
+      />;
+    })
+
+    appointmentList.push(<Appointment key="last" time="5pm" />);
+
+    return appointmentList;
+  }
 
   return (
     <main className="layout">
@@ -47,7 +100,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
+        {showAppointments()}
       </section>
     </main>
   );
