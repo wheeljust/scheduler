cc4a2f7417492d63ee1b8dd003cda881ac1eef87
Justin Wheeler <justinwwheeler@gmail.com>
2021-11-15 19:35:10 -0700

    add the Form component to the Appointment and update stories

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Button from '../Button';
+import InterviewerList from '../InterviewerList';
+
+export default function Form(props) {
+  const { onSave, onCancel } = props;
+
+  //If the form receives state values for the student and interviewer, then these values should appear initially
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  //Reset the state values in the form
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  //Reset the form and call whatever function is passed to the form as onCancel
+  const cancel = () => {
+    reset();
+    onCancel();
+  }
+
+
+  //********** NOTE - the onSave function will be updated in a later exercise ************************** //
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          value={interviewer}
+          onChange={setInterviewer}
+          interviewers={props.interviewers}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -168,5 +169,21 @@ storiesOf("Appointment", module)
     <Error
       message={"Could not delete appointment"}
       onClose={action("onClose")}
+    />
+  ))
+  .add("Form_Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form_Edit", () => (
+    <Form
+      student={"Justin Wheeler"}
+      interviewer={2}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
