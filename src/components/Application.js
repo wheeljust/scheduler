7969fd5a094b7d8fff3c4f3e2cd344a36e6a623e
Justin Wheeler <justinwwheeler@gmail.com>
2021-11-18 17:53:53 -0700

    refactor state into one single object called state and useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,8 +44,14 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    //appointments: {}
+  })
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get('/api/days')
@@ -78,8 +84,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
