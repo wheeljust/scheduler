d7f3ff2cfc6dd84e1841467a4a843246e9b8cc62
Justin Wheeler <justinwwheeler@gmail.com>
2021-11-18 20:25:13 -0700

    add a getInterview selector to parse individual interviewer data from the state object, all tests passing

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,14 @@
+/**
+ * getAppointmentsForDay
+ * @param {Object} state includes key value pairs of the tracked state
+ * @param {String} selectedDay the current value of the "day" state
+ * @returns {Array} containing appointment objects related to the selectedDay
+ */
 export const getAppointmentsForDay = (state, selectedDay) => {
   const { days, appointments } = state;
 
   // If no days data was found, return an empty array
-  if (!days.length) {
-    return [];
-  }
+  if (!days.length) return [];
 
   let appointmentArray;
   for (const day of days) {
@@ -15,9 +19,7 @@ export const getAppointmentsForDay = (state, selectedDay) => {
   };
 
   // No day.name in the daysArray matching the selected day
-  if (!appointmentArray) {
-    return [];
-  }
+  if (!appointmentArray) return [];
 
   const appointmentDetails = appointmentArray.reduce((results, id) => {
     results.push(appointments[id]);
@@ -25,4 +27,19 @@ export const getAppointmentsForDay = (state, selectedDay) => {
   }, [])
 
   return appointmentDetails;
-}
+};
+
+/**
+ * getInterview
+ * @param {Object} state includes key value pairs of the tracked state
+ * @param {Object} interview consists of the students name and the interviewer's id
+ * @returns {Object} revised version of the interview, with the interviewers details fetched from the state.interviewers
+ */
+export const getInterview = (state, interview) => {
+  if (!interview) return null;
+
+  const interviewerID = interview.interviewer;
+  const newInterview = { ...interview, interviewer: state.interviewers[interviewerID] }
+
+  return newInterview
+};
