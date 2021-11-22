15dbe8c2e2e9742898a316fc4dfbf743f30b0ed2
Justin Wheeler <justinwwheeler@gmail.com>
2021-11-22 16:34:21 -0700

    add updateSpotsRemaining function and call in bookInterview and cancelInterview

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -44,8 +44,10 @@ export default function useApplicationData() {
       [id]: appointment
     };
 
+    const days = updateSpotsRemaining(state.days, state.day, -1);
+
     return axios.put(`/api/appointments/${id}`, { interview })
-      .then(() => setState({ ...state, appointments }))
+      .then(() => setState({ ...state, appointments, days }))
   };
 
   /**
@@ -58,14 +60,32 @@ export default function useApplicationData() {
       ...state.appointments[id],
       interview: null
     };
+
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
 
+    const days = updateSpotsRemaining(state.days, state.day, 1);
+
     return axios.delete(`/api/appointments/${id}`)
-      .then(() => setState({ ...state, appointments }));
+      .then(() => setState({ ...state, appointments, days }));
   };
+
+  /**
+   * updateSpotsRemaining
+   * @param {Array} days 
+   * @param {String} day 
+   * @param {Integer} changeAmt 
+   * @returns a new days array with the updatedSpots remaining for the current day
+   */
+  const updateSpotsRemaining = (days, day, changeAmt) => {
+    const dayIndex = days.findIndex(currentDay => currentDay.name === day);
+    const currentSpots = days[dayIndex].spots
+
+    days[dayIndex].spots = currentSpots + changeAmt;
+    return days;
+  }
 
   return {
     state,
