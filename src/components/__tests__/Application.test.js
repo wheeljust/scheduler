1ca851c5ad753cf9e0cb7901e18816fcfd01ff84
Justin Wheeler <justinwwheeler@gmail.com>
2021-12-03 21:09:40 -0700

    add integration test for the complete user booking new interview experience

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,18 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  waitForElementToBeRemoved,
+  fireEvent,
+  queryByText,
+  getByText,
+  getByAltText,
+  getByPlaceholderText,
+  getAllByTestId,
+  prettyDOM
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -16,6 +28,40 @@ describe("Application", () => {
         fireEvent.click(getByText("Tuesday"));
         expect(getByText("Leopold Silvers")).toBeInTheDocument();
       })
+  });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    // 1. Render the Application component, the container will used for scoping queries
+    const { container, debug } = render(<Application />);
+
+    // 2. Wait for the mock axios get requests to resolve
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Target the first appointment article on the page, located at index 0 of the response from the getAllByTestId query
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    // 4. Select add to show the create new appointment form
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    // 5.  Change the student value, click interviewer, click save
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+    expect(getByText(appointment, /saving/i)).toBeInTheDocument();
+
+    // 6. Wait for the mock axios put call to resolve then check for the added students name on the page
+    await waitForElementToBeRemoved(() => getByText(appointment, /saving/i));
+    expect(getByText(container, "Lydia Miller-Jones")).toBeInTheDocument();
+
+    // 7. Target the day list, and QUERY for the list item with text "Monday"
+    const days = getAllByTestId(container, "day");
+    const day = days.find(day => queryByText(day, "Monday"))
+
+    // 8. Test that the day item with text Monday also has text "no spots remaining"
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
 
