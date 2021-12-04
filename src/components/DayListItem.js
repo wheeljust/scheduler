c3458f183e20279e3ac1a9316b1502c13326366b
Justin Wheeler <justinwwheeler@gmail.com>
2021-12-03 21:09:08 -0700

    add testid to the daylistitem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -19,6 +19,7 @@ export default function DayListItem(props) {
     <li
       onClick={setDay}
       className={dayClass}
+      data-testid="day"
     >
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
